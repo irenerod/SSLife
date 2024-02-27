bd960e07e20a6ba5e44359e5039a116f6a13b1c5
irenerod <irenerodenas.alu@iespacomolla.es>
2024-02-27 22:23:21 +0100

    Caelestis
    Caelestis segundo intento.

---
+++ src/componentes/Caelestis/Caelestis.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Caelestis.css';
+
+const Caelestis = () => {
+  return (
+    <section className="contenedorCaelestis">
+    <section className="caelestis">
+      <div className="uno"></div>
+      <p>Caelestis</p>
+      <div className="tres"></div>
+    </section>
+    </section>
+  );
+};
+
+export default Caelestis;
